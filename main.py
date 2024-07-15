d6d968beca66b2d7416e61e63b0452d83be54561
bucciadimela2-0 <martina.buccioni@stud.unifi.it>
2024-07-15 11:58:35 +0200

    Added mutation logic and ALK-EML4 example

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
-### DNA walks: deepening our knowledge on wild-type genes ###
+### DNA walks: deepening our knowledge on wild-type genes and mutated genes ###
 ###### An example on lung cancer genes ######
-
 
 # Main libraries
 
@@ -19,28 +18,44 @@ from PIL import Image
 from skimage import measure
 from svg_turtle import SvgTurtle
 
-#Downloading and parsing of the fasta gene file
 
-########Just to understand Bio #########
-file_path = '/Users/kepler/Desktop/StatisticaFisica/DnaWalk/dataset/ALK.fasta'
-c  = 0
-for seq_record in SeqIO.parse(file_path, "fasta"):
-    print(f"ID: {seq_record.id}")
-    print(f"Name: {seq_record.name}")
-    print(f"Description: {seq_record.description}")
-    print(f"Annotations: {seq_record.annotations}")
-    print(f"Features: {seq_record.features}")
-    print(f"Sequence Data: {repr(seq_record.seq)}")
-    print(f"Length: {len(seq_record)}")
-    print("\n")
-    c+= 1  
+########Reading and parsing of the fasta gene file########
+def read_fasta(file_path):
 
-print(f"Quantity of gene in the chosen database: {c}")
+    c  = 0
+    for seq_record in SeqIO.parse(file_path, "fasta"):
+        print(f"ID: {seq_record.id}")
+        print(f"Name: {seq_record.name}")
+        print(f"Description: {seq_record.description}")
+        print(f"Annotations: {seq_record.annotations}")
+        print(f"Features: {seq_record.features}")
+        print(f"Sequence Data: {repr(seq_record.seq)}")
+        print(f"Length: {len(seq_record)}")
+        print("\n")
+        c+= 1  
 
-####### Parsing the ALK sequence ########
+    print(f"Quantity of gene in the chosen database: {c}")
 
-alk_seq = seq_record.seq
-print(f"Sequence Data: {alk_seq}")
+    return seq_record
+
+########Extracting interested exons ########
+def extract_exons(file_path, exons, direction):
+
+    finalsequence = " "
+    c = 0
+    
+    for seq_record in SeqIO.parse(file_path, "fasta"):
+        c =  c + 1
+
+        if direction == 'fd':
+                if c <= exons:
+                    finalsequence = finalsequence + seq_record.seq
+                    
+        elif direction == 'bw':
+                if c >= exons:
+                    finalsequence = finalsequence + seq_record.seq
+    
+    return finalsequence    
 
 
 ######Defining the DNA Walk ######
@@ -60,54 +75,70 @@ def dna_walk(sequence):
         walk.append((x, y))
     return walk
 
-
-#### Printing the ALK walk #####
-alk_walk = dna_walk(alk_seq)
-print(f"this is the DNA walk: {alk_walk} ")
-
-
 ##### Plotting the DNA Walk using Turtle ######
 
-def plot_dna_walk(walk):
+def plot_dna_walk(walk, name):
     t = SvgTurtle(300, 300)
-    output_file = '/Users/kepler/Desktop/StatisticaFisica/DnaWalk/Images/ALK/ALK.ps'
+    output_file = '/Users/kepler/Desktop/StatisticaFisica/DnaWalk/Images/'+name
     window = turtle.Screen()
     window.bgcolor("white")
     pen = turtle.Turtle()
+    turtle.hideturtle
     pen.speed(1000)
-
     for x, y in walk:
         pen.goto(x*1.5, y*1.5)  
-    
-    turtle.getscreen().getcanvas().postscript(file = output_file)
+    turtle.getscreen().getcanvas().postscript(file = output_file+'.ps')
     print(f"Immagine della camminata del DNA salvata in {output_file}")
-
-
-    
+    convert_to_png(output_file)
     window.exitonclick()
 
 ##### Converting the output file in PNG file #####
 
 def convert_to_png(path):
-    img = Image.open(path)
-    img.save('/Users/kepler/Desktop/StatisticaFisica/DnaWalk/Images/ALK/ALK.png')
+    img = Image.open(path+'.ps')
+    img.save(path+'.png')
     print(f"Immagine convertita da .ps a .png")
 
-##### Plotting The ALK WALK ########
+####### Parsing the ALK sequence ########
+
+file_path = '/Users/kepler/Desktop/StatisticaFisica/DnaWalk/dataset/ALK.fasta'
+
+alk_seq = read_fasta(file_path)
+print(f"Sequence Data: {alk_seq}")
+
+#### Printing the ALK walk #####
+
+alk_walk = dna_walk(alk_seq)
+print(f"this is the DNA walk: {alk_walk} ")
 
 output_file = '/Users/kepler/Desktop/StatisticaFisica/DnaWalk/Images/ALK/ALK.ps'
 
-plot_dna_walk(alk_walk)
-convert_to_png(output_file)
+#### Plotting the ALK walk and converting to png ####
+
+#plot_dna_walk(alk_walk, 'alk_walk')
 
 
+#####Building the Mutation string #####
+### reading the files, extracting the interested exons ###
 
+exons_elm4_path = '/Users/kepler/Desktop/StatisticaFisica/DnaWalk/dataset/EML4Exons.fa'
+read_fasta(exons_elm4_path)
+exons_eml4_13 = extract_exons(exons_elm4_path, 13, 'fd')
+print(exons_eml4_13)
+#exons_eml4_walk = dna_walk(exons_eml4_13)
+#plot_dna_walk(exons_eml4_walk)
 
-######
+exons_alk_path = '/Users/kepler/Desktop/StatisticaFisica/DnaWalk/dataset/ALKexons.fa'
+read_fasta(exons_alk_path)
+exons_alk_20 = extract_exons(exons_alk_path, 20, 'bw')
+print(exons_alk_20)
+#exons_alk_walk = dna_walk(exons_alk_20)
+#plot_dna_walk(exons_alk_walk)
 
+### Building and printing the mutated gene ###
+alk_eml4_mutation = exons_eml4_13 + exons_alk_20
+print(alk_eml4_mutation)
 
-
-
-
-
+alk_eml4_walk = dna_walk(alk_eml4_mutation)
+plot_dna_walk(alk_eml4_walk, 'alk_eml4_walk')
 
